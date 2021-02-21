9a7e8e6731c637da7490f9904d22777e342ad2d4
John-Paul Harold <johnpaul.harold@gmail.com>
2021-02-21 10:53:25 +0000

    add optional chaning babel plugin

--- babel.config.js
+++ babel.config.js
@@ -27,6 +27,7 @@ module.exports = api => {
       '@babel/plugin-proposal-object-rest-spread',
       '@babel/plugin-transform-destructuring',
       '@babel/plugin-proposal-nullish-coalescing-operator',
+      "@babel/plugin-proposal-optional-chaining"
     ],
     presets: [
       ['@babel/preset-env', config[configKey]],
