f1f5299c082218afb15d83d5065c9aed74554a7d
James Smith <jamesplease2@gmail.com>
2021-06-17 16:21:15 -0400

    Add more tests

--- src/tests/focus-node-events.test.js
+++ src/tests/focus-node-events.test.js
@@ -4,6 +4,78 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import { FocusRoot, FocusNode, useFocusStoreDangerously } from '../index';
 
 describe('FocusNode Events', () => {
+  describe('onMove', () => {
+    it('calls it when movement occurs', () => {
+      const rootOnMove = jest.fn();
+      const nodeAOnMove = jest.fn();
+      const nodeBOnMove = jest.fn();
+      let focusStore;
+
+      function TestComponent() {
+        focusStore = useFocusStoreDangerously();
+
+        return (
+          <FocusNode onMove={rootOnMove}>
+            <FocusNode
+              focusId="nodeA"
+              onMove={nodeAOnMove}
+              data-testid="nodeA"
+            />
+            <FocusNode
+              focusId="nodeB"
+              data-testid="nodeB"
+              onMove={nodeBOnMove}
+            />
+          </FocusNode>
+        );
+      }
+
+      render(
+        <FocusRoot>
+          <TestComponent />
+        </FocusRoot>
+      );
+
+      expect(focusStore.getState().focusedNodeId).toEqual('nodeA');
+
+      expect(rootOnMove.mock.calls.length).toBe(0);
+      expect(nodeAOnMove.mock.calls.length).toBe(0);
+      expect(nodeBOnMove.mock.calls.length).toBe(0);
+
+      // No movement, so no events fire
+      fireEvent.keyDown(window, {
+        code: 'ArrowLeft',
+        key: 'ArrowLeft',
+      });
+
+      expect(rootOnMove.mock.calls.length).toBe(0);
+      expect(nodeAOnMove.mock.calls.length).toBe(0);
+      expect(nodeBOnMove.mock.calls.length).toBe(0);
+
+      // Moving right
+      fireEvent.keyDown(window, {
+        code: 'ArrowRight',
+        key: 'ArrowRight',
+      });
+
+      expect(rootOnMove.mock.calls.length).toBe(1);
+      expect(nodeAOnMove.mock.calls.length).toBe(0);
+      expect(nodeBOnMove.mock.calls.length).toBe(0);
+
+      expect(rootOnMove).toHaveBeenCalledWith(
+        expect.objectContaining({
+          orientation: 'horizontal',
+          direction: 'forward',
+          arrow: 'right',
+          prevChildIndex: 0,
+          nextChildIndex: 1,
+          prevChildNode: focusStore.getState().nodes.nodeA,
+          nextChildNode: focusStore.getState().nodes.nodeB,
+        })
+      );
+    });
+  });
+
   describe('onBlur/onFocus', () => {
     it('calls them when appropriate', () => {
       const nodeAOnFocused = jest.fn();
@@ -150,6 +222,144 @@ describe('FocusNode Events', () => {
       focusState = focusStore.getState();
       expect(focusState.focusedNodeId).toEqual('nodeB');
       expect(focusState.focusHierarchy).toEqual(['root', 'nodeB']);
+    });
+  });
+
+  describe('onSelected', () => {
+    it('calls it when selection occurs', () => {
+      const rootOnSelected = jest.fn();
+      const nodeAOnSelected = jest.fn();
+      const nodeBOnSelected = jest.fn();
+      let focusStore;
+
+      function TestComponent() {
+        focusStore = useFocusStoreDangerously();
+
+        return (
+          <FocusNode focusId="testRoot" onSelected={rootOnSelected}>
+            <FocusNode
+              focusId="nodeA"
+              onSelected={nodeAOnSelected}
+              data-testid="nodeA"
+            />
+            <FocusNode
+              focusId="nodeB"
+              data-testid="nodeB"
+              onSelected={nodeBOnSelected}
+            />
+          </FocusNode>
+        );
+      }
+
+      render(
+        <FocusRoot>
+          <TestComponent />
+        </FocusRoot>
+      );
+
+      expect(focusStore.getState().focusedNodeId).toEqual('nodeA');
+
+      expect(rootOnSelected.mock.calls.length).toBe(0);
+      expect(nodeAOnSelected.mock.calls.length).toBe(0);
+      expect(nodeBOnSelected.mock.calls.length).toBe(0);
+
+      const preFocusedNodeA = focusStore.getState().nodes.nodeA;
+
+      fireEvent.keyDown(window, {
+        code: 'Enter',
+        key: 'Enter',
+      });
+
+      expect(rootOnSelected.mock.calls.length).toBe(1);
+      expect(nodeAOnSelected.mock.calls.length).toBe(1);
+      expect(nodeBOnSelected.mock.calls.length).toBe(0);
+
+      expect(rootOnSelected).toHaveBeenCalledWith(
+        expect.objectContaining({
+          key: 'select',
+          isArrow: false,
+          node: focusStore.getState().nodes.testRoot,
+        })
+      );
+
+      expect(nodeAOnSelected).toHaveBeenCalledWith(
+        expect.objectContaining({
+          key: 'select',
+          isArrow: false,
+          // The event fires before the state updates to reflect the new
+          // active state. This allows you to, say, prevent default
+          // the action.
+          node: preFocusedNodeA,
+        })
+      );
+    });
+  });
+
+  describe('onBack', () => {
+    it.skip('calls it when Esc is pressed', () => {
+      const rootonBack = jest.fn();
+      const nodeAonBack = jest.fn();
+      const nodeBonBack = jest.fn();
+      let focusStore;
+
+      function TestComponent() {
+        focusStore = useFocusStoreDangerously();
+
+        return (
+          <FocusNode focusId="testRoot" onBack={rootonBack}>
+            <FocusNode
+              focusId="nodeA"
+              onBack={nodeAonBack}
+              data-testid="nodeA"
+            />
+            <FocusNode
+              focusId="nodeB"
+              data-testid="nodeB"
+              onBack={nodeBonBack}
+            />
+          </FocusNode>
+        );
+      }
+
+      render(
+        <FocusRoot>
+          <TestComponent />
+        </FocusRoot>
+      );
+
+      expect(focusStore.getState().focusedNodeId).toEqual('nodeA');
+
+      expect(rootonBack.mock.calls.length).toBe(0);
+      expect(nodeAonBack.mock.calls.length).toBe(0);
+      expect(nodeBonBack.mock.calls.length).toBe(0);
+
+      fireEvent.keyDown(window, {
+        code: 'Esc',
+        key: 'Esc',
+      });
+
+      expect(rootonBack.mock.calls.length).toBe(1);
+      expect(nodeAonBack.mock.calls.length).toBe(1);
+      expect(nodeBonBack.mock.calls.length).toBe(0);
+
+      // expect(rootonBack).toHaveBeenCalledWith(
+      //   expect.objectContaining({
+      //     key: 'select',
+      //     isArrow: false,
+      //     node: focusStore.getState().nodes.testRoot,
+      //   })
+      // );
+
+      // expect(nodeAonBack).toHaveBeenCalledWith(
+      //   expect.objectContaining({
+      //     key: 'select',
+      //     isArrow: false,
+      //     // The event fires before the state updates to reflect the new
+      //     // active state. This allows you to, say, prevent default
+      //     // the action.
+      //     node: preFocusedNodeA,
+      //   })
+      // );
     });
   });
 });
