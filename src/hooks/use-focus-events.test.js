f783ae2a8e2fe228d553020031f280209e8db592
James Smith <jamesplease2@gmail.com>
2021-06-17 11:00:47 -0400

    Add enabled/disabled focus events tests

--- src/hooks/use-focus-events.test.js
+++ src/hooks/use-focus-events.test.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@testing-library/jest-dom';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, act } from '@testing-library/react';
 import {
   FocusRoot,
   FocusNode,
@@ -9,7 +9,7 @@ import {
 } from '../index';
 
 describe('useFocusEvents', () => {
-  describe('onBlur/onFocus', () => {
+  describe('focus/blur', () => {
     it('calls them when appropriate', () => {
       const nodeAOnFocused = jest.fn();
       const nodeAOnBlurred = jest.fn();
@@ -67,5 +67,67 @@ describe('useFocusEvents', () => {
       expect(nodeBOnFocused.mock.calls.length).toBe(1);
       expect(nodeBOnBlurred.mock.calls.length).toBe(0);
     });
+  });
+
+  it('disabled/enabled (enabled by default)', () => {
+    const nodeAOnDisabled = jest.fn();
+    const nodeAOnEnabled = jest.fn();
+    const nodeBOnDisabled = jest.fn();
+    const nodeBOnEnabled = jest.fn();
+    let focusStore;
+    let updateDisableA;
+
+    function TestComponent() {
+      const [disableA, setDisableA] = useState(false);
+      focusStore = useFocusStoreDangerously();
+
+      updateDisableA = setDisableA;
+
+      useFocusEvents('nodeA', {
+        disabled: nodeAOnDisabled,
+        enabled: nodeAOnEnabled,
+      });
+
+      useFocusEvents('nodeB', {
+        disabled: nodeBOnDisabled,
+        enabled: nodeBOnEnabled,
+      });
+
+      return (
+        <>
+          <FocusNode focusId="nodeA" data-testid="nodeA" disabled={disableA} />
+          <FocusNode focusId="nodeB" data-testid="nodeB" />
+        </>
+      );
+    }
+
+    render(
+      <FocusRoot>
+        <TestComponent />
+      </FocusRoot>
+    );
+
+    let focusState = focusStore.getState();
+    expect(focusState.focusedNodeId).toEqual('nodeA');
+    expect(focusState.focusHierarchy).toEqual(['root', 'nodeA']);
+
+    expect(nodeAOnDisabled.mock.calls.length).toBe(0);
+    expect(nodeAOnEnabled.mock.calls.length).toBe(0);
+    expect(nodeBOnDisabled.mock.calls.length).toBe(0);
+    expect(nodeBOnEnabled.mock.calls.length).toBe(0);
+
+    act(() => updateDisableA(true));
+
+    expect(nodeAOnDisabled.mock.calls.length).toBe(1);
+    expect(nodeAOnEnabled.mock.calls.length).toBe(0);
+    expect(nodeBOnDisabled.mock.calls.length).toBe(0);
+    expect(nodeBOnEnabled.mock.calls.length).toBe(0);
+
+    act(() => updateDisableA(false));
+
+    expect(nodeAOnDisabled.mock.calls.length).toBe(1);
+    expect(nodeAOnEnabled.mock.calls.length).toBe(1);
+    expect(nodeBOnDisabled.mock.calls.length).toBe(0);
+    expect(nodeBOnEnabled.mock.calls.length).toBe(0);
   });
 });
