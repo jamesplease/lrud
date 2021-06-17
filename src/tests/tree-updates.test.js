7c1a8e81ce2af048128153a4d7526e80ac4f9795
James Smith <jamesplease2@gmail.com>
2021-06-17 16:53:33 -0400

    Test updates with grids

--- src/tests/tree-updates.test.js
+++ src/tests/tree-updates.test.js
@@ -120,4 +120,113 @@ describe('Tree updates', () => {
     expect(focusState.activeNodeId).toEqual(null);
     expect(Object.values(focusState.nodes)).toHaveLength(4);
   });
+
+  it('respects `onMountAssignFocusTo`', () => {
+    let focusStore;
+    let updateMountState;
+
+    function TestComponent() {
+      focusStore = useFocusStoreDangerously();
+      const [mountChild, setMountChild] = useState(false);
+      updateMountState = setMountChild;
+
+      return (
+        <FocusNode focusId="nodeA" data-testid="nodeA">
+          {mountChild && (
+            <FocusNode
+              focusId="nodeA-A"
+              data-testid="nodeA-A"
+              onMountAssignFocusTo="nodeA-A-B">
+              <FocusNode focusId="nodeA-A-A" data-testid="nodeA-A-A" />
+              <FocusNode focusId="nodeA-A-B" data-testid="nodeA-A-B" />
+            </FocusNode>
+          )}
+        </FocusNode>
+      );
+    }
+
+    render(
+      <FocusRoot>
+        <TestComponent />
+      </FocusRoot>
+    );
+
+    let focusState = focusStore.getState();
+    expect(focusState.focusedNodeId).toEqual('nodeA');
+    expect(focusState.focusHierarchy).toEqual(['root', 'nodeA']);
+    expect(focusState.activeNodeId).toEqual(null);
+    expect(Object.values(focusState.nodes)).toHaveLength(2);
+
+    act(() => updateMountState(true));
+
+    focusState = focusStore.getState();
+    expect(focusState.focusedNodeId).toEqual('nodeA-A-B');
+    expect(focusState.focusHierarchy).toEqual([
+      'root',
+      'nodeA',
+      'nodeA-A',
+      'nodeA-A-B',
+    ]);
+    expect(focusState.activeNodeId).toEqual(null);
+    expect(Object.values(focusState.nodes)).toHaveLength(5);
+  });
+
+  it('works with grids', () => {
+    let focusStore;
+    let updateMountState;
+
+    function TestComponent() {
+      focusStore = useFocusStoreDangerously();
+      const [mountChild, setMountChild] = useState(false);
+      updateMountState = setMountChild;
+
+      return (
+        <FocusNode focusId="testRoot">
+          {mountChild && (
+            <FocusNode
+              focusId="gridRoot"
+              data-testid="gridRoot"
+              isGrid
+              defaultFocusColumn={1}
+              defaultFocusRow={1}>
+              <FocusNode focusId="gridRow1">
+                <FocusNode focusId="gridItem1-1" />
+                <FocusNode focusId="gridItem1-2" />
+              </FocusNode>
+              <FocusNode focusId="gridRow2">
+                <FocusNode focusId="gridItem2-1" />
+                <FocusNode focusId="gridItem2-2" />
+              </FocusNode>
+            </FocusNode>
+          )}
+        </FocusNode>
+      );
+    }
+
+    render(
+      <FocusRoot>
+        <TestComponent />
+      </FocusRoot>
+    );
+
+    let focusState = focusStore.getState();
+    expect(focusState.focusedNodeId).toEqual('testRoot');
+    expect(focusState.focusHierarchy).toEqual(['root', 'testRoot']);
+    expect(focusState.activeNodeId).toEqual(null);
+    expect(Object.values(focusState.nodes)).toHaveLength(2);
+
+    act(() => updateMountState(true));
+
+    focusState = focusStore.getState();
+    expect(focusState.focusedNodeId).toEqual('gridItem2-2');
+    expect(focusState.focusHierarchy).toEqual([
+      'root',
+      'testRoot',
+      'gridRoot',
+      'gridRow2',
+      'gridItem2-2',
+    ]);
+    expect(focusState.activeNodeId).toEqual(null);
+    expect(Object.values(focusState.nodes)).toHaveLength(9);
+  });
 });
