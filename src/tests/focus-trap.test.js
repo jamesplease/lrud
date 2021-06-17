60017cd73487f0208dafe666f4b49e8d34cccb8e
James Smith <jamesplease2@gmail.com>
2021-06-17 13:58:26 -0400

    Begin authoring focus trap tests

--- src/tests/focus-trap.test.js
+++ src/tests/focus-trap.test.js
@@ -25,4 +25,31 @@ describe('Focus Traps', () => {
     expect(warning).toHaveBeenCalledTimes(1);
     expect(warning.mock.calls[0][1]).toEqual('RESTORE_TRAP_FOCUS_WITHOUT_TRAP');
   });
+
+  it.skip('does not focus on mount', () => {
+    let focusStore;
+
+    function TestComponent() {
+      focusStore = useFocusStoreDangerously();
+
+      return (
+        <FocusNode focusId="nodeA" data-testid="nodeA" isTrap>
+          <FocusNode focusId="nodeA-A" data-testid="nodeA-A">
+            <FocusNode focusId="nodeA-A-A" data-testid="nodeA-A-A">
+              A
+            </FocusNode>
+          </FocusNode>
+          <FocusNode focusId="nodeA-B" data-testid="nodeA-B" />
+        </FocusNode>
+      );
+    }
+
+    render(
+      <FocusRoot>
+        <TestComponent />
+      </FocusRoot>
+    );
+
+    expect(focusStore.getState().focusedNodeId).toEqual('root');
+  });
 });
