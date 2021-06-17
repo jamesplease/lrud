64bc83bda47af6cbcb8c69a144c735976fa39e10
James Smith <jamesplease2@gmail.com>
2021-06-17 12:11:32 -0400

    Begin adding focus node tests

---
+++ src/focus-node.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, act, fireEvent, screen } from '@testing-library/react';
+import {
+  FocusRoot,
+  FocusNode,
+  useSetFocus,
+  useFocusStoreDangerously,
+} from './index';
+import { warning } from './utils/warning';
+
+describe('FocusNode', () => {
+  it('warns when there is no FocusRoot', () => {
+    function TestComponent() {
+      return (
+        <>
+          <FocusNode focusId="nodeA" data-testid="nodeA" />
+        </>
+      );
+    }
+
+    expect(() => {
+      render(<TestComponent />);
+    }).toThrow();
+
+    expect(warning).toHaveBeenCalledTimes(2);
+    expect(warning.mock.calls[0][1]).toEqual('NO_FOCUS_PROVIDER_DETECTED');
+    // Note: I'm not entirely sure why the warning is called twice in this test...but that's OK
+    expect(warning.mock.calls[1][1]).toEqual('NO_FOCUS_PROVIDER_DETECTED');
+  });
+});
