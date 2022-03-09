fdc0469463b3e94dca84a501a0df267dfad71a1d
James Please <deeper_halal0y@icloud.com>
2022-03-09 12:18:48 -0500

    Remove useFocusNode alias

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import FocusContext from './focus-context';
 export { default as FocusNode } from './focus-node';
 export { default as useFocusNodeById } from './hooks/use-focus-node-by-id';
-export { default as useFocusNode } from './hooks/use-focus-node';
 export { default as useLeafFocusedNode } from './hooks/use-leaf-focused-node';
 export { default as useActiveNode } from './hooks/use-active-node';
 export { default as useFocusHierarchy } from './hooks/use-focus-hierarchy';
